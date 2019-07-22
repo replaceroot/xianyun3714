44aefc708cc58ad55eca43c71f9e62fd91323cb6
2977he <2977540255@qq.com>
2019-07-22 09:20:27 +0800

    发表新攻略静态和部分功能已完成

--- pages/post/index.vue
+++ pages/post/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div style="padding:50px;">
-     <h1 style="margin-bottom:50px;">组件递归</h1>
+     <!-- <h1 style="margin-bottom:50px;">组件递归</h1>
 
-    <NavsItem :data="navs"/>
-
+    <NavsItem :data="navs"/> -->
+     <el-button type="primary" @click="handleClick">写游记</el-button>
   </div>
 </template>
 
@@ -15,50 +15,55 @@ export default {
     components: {
         NavsItem,
     },
-    data(){
-        return {
-            navs: [
-                {
-                    title: "衣服",
-                    children: [
-                        { 
-                            title: "男装",
-                            children: [
-                                { 
-                                    title: "T恤",
-                                    children: [ 
-                                        {title: "短袖"}, 
-                                        { title: "长袖" }
-                                     ]
-                                }
-                            ]
-                        },
-                        { 
-                            title: "女装",
-                            children: [
-                                {
-                                    title: "裙子",
-                                    children: [
-                                        { title: "连衣裙" },
-                                        { title: "短裙" }
-                                    ]
-                                }
-                            ]
-                        }
-                    ]
-                },
-                {
-                    title: "电器",
-                    children: [
-                        { title: "电视", children: [
-                            { title: "液晶电视" },
-                            { title: "普通电视"}
-                        ] },
-                        { title: "冰箱" },
-                        { title: "空调" },
-                    ]
-                }
-            ]
+    // data(){
+    //     return {
+    //         navs: [
+    //             {
+    //                 title: "衣服",
+    //                 children: [
+    //                     { 
+    //                         title: "男装",
+    //                         children: [
+    //                             { 
+    //                                 title: "T恤",
+    //                                 children: [ 
+    //                                     {title: "短袖"}, 
+    //                                     { title: "长袖" }
+    //                                  ]
+    //                             }
+    //                         ]
+    //                     },
+    //                     { 
+    //                         title: "女装",
+    //                         children: [
+    //                             {
+    //                                 title: "裙子",
+    //                                 children: [
+    //                                     { title: "连衣裙" },
+    //                                     { title: "短裙" }
+    //                                 ]
+    //                             }
+    //                         ]
+    //                     }
+    //                 ]
+    //             },
+    //             {
+    //                 title: "电器",
+    //                 children: [
+    //                     { title: "电视", children: [
+    //                         { title: "液晶电视" },
+    //                         { title: "普通电视"}
+    //                     ] },
+    //                     { title: "冰箱" },
+    //                     { title: "空调" },
+    //                 ]
+    //             }
+    //         ]
+    //     }
+    // },
+    methods:{
+        handleClick(){
+            this.$router.push("/post/create")
         }
     }
 }
